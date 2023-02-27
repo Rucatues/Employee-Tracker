e4c06e51908ab2082d3d31029d0b9cc7e28096d1
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-02-27 12:14:24 -0600

    comments added for each function based on homework requirements

--- index.js
+++ index.js
@@ -45,14 +45,20 @@ function menu() {
 
 function viewAllEmployees() {
     console.log("viewing all employees");
+    // WHEN I choose to view all employees
+    // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 };
 
 function addEmployee() {
     console.log("added employee");
+    // WHEN I choose to add an employee
+    // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 };
 
 function updateEmployee() {
     console.log("updated employee");
+    // WHEN I choose to update an employee role
+    // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 };
 
 function viewAllRoles() {
@@ -63,6 +69,8 @@ function viewAllRoles() {
 
 function addRole() {
     console.log("added role");
+    // WHEN I choose to add a role
+    // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 };
 
 function viewAllDepartments() {
@@ -73,10 +81,13 @@ function viewAllDepartments() {
 
 function addDepartment() {
     console.log("added department");
+    // WHEN I choose to add a department
+    // THEN I am prompted to enter the name of the department and that department is added to the database
 };
 
 function finish() {
     console.log("finished with menu");
+    // What to do when finished? Just display all data? 
 };
 
 menu();
