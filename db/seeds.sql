f264af9846e0c98bb02a89672a4f94888f5d9187
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-01 21:42:07 -0600

    changed table names in seeds to match schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
-INSERT INTO department (department_name)
+INSERT INTO departments (department_name)
 VALUES ("Dummy Dept Name 1"),
        ("Dummy Dept Name 2"),
        ("Dummy Dept Name 3"),
        ("Dummy Dept Name 4"),
        ("Dummy Dept Name 5");
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO roles (id, title, salary, department_id)
 VALUES (001, "Manager", 100000.00, 1111),
        (002, "Senior Dev", 120000.00, 2222),
        (003, "Junior Dev", 80000.00, 3333),
        (004, "Intern", 50000.00, 4444); 
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES (001, "Roger", "Smith", ),
        (002, "Senior Dev"),
        (003, "Junior Dev"),
