20101969434525530c330a3808f00b67891acbef
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-01 20:07:13 -0600

    added keys to tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,31 +5,34 @@ CREATE DATABASE employees_db;
 USE employees_db; 
 
 CREATE TABLE department (
-     -- id: INT PRIMARY KEY
-
+     id INT NOT NULL,
+    --  how to make id primary Key? 
+    name VARCHAR(30) NOT NULL,
     -- name: VARCHAR(30) to hold department name
 );
 
 CREATE TABLE role (
-        -- id: INT PRIMARY KEY
-
+     id INT NOT NULL,
+    --  how to make id primary Key? 
+    title VARCHAR(30) NOT NULL,
         -- title: VARCHAR(30) to hold role title
-
+    salary DECIMAL NOT NULL,
         -- salary: DECIMAL to hold role salary
-
+    department_id INT NOT NULL
         -- department_id: INT to hold reference to department role belongs to
 ); 
 
 
 CREATE TABLE employee (
-  -- id: INT PRIMARY KEY
-
+     id INT NOT NULL,
+    --  how to make id primary Key? 
+    first_name VARCHAR(30) NOT NULL, 
         -- first_name: VARCHAR(30) to hold employee first name
-
+    last_name VARCHAR(30) NOT NULL,
         -- last_name: VARCHAR(30) to hold employee last name
-
+    role_id INT NOT NULL,
         -- role_id: INT to hold reference to employee role
-
+    manager_id INT, 
         -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 );
 
