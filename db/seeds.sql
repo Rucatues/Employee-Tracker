a9539aa24328d845df27fcc7e5f8d325e959bc3b
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-01 21:51:07 -0600

    missing comma added on line 10

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ INSERT INTO roles (title, salary, department_id)
 VALUES ("Clinic Manager", 1000000, 1),
        ("Nurse", 70000, 2),
        ("Medical Assistant", 40000, 2),
-       ("Sonography Manager", 90000, 3)
+       ("Sonography Manager", 90000, 3),
        ("Sonographer", 70000, 3); 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
