3acee87376f4a953855547350c05f9b927a28a4c
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-04 09:16:37 -0600

    viewAllRoles function working

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const db = mysql.createConnection({
     console.log("You are now connected to the hospital employees database")
 );
 
+let roleID = '';
+let manager = '';
 // ====================Main Menu====================
 function menu() {
     inquirer.prompt([
@@ -58,19 +60,90 @@ function menu() {
                 finish();
             }
         })
-}
+};
 
 // ====================Menu options====================
 function viewAllEmployees() {
     console.log("viewing all employees");
     // WHEN I choose to view all employees
     // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    db.query("SELECT * FROM employees;"),
+        function (err, res) {
+            if (err) {
+                console.log("ERROR!");
+                console.log(err);
+            }
+            console.table(res);
+            menu();
+        }
 };
 
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 function addEmployee() {
-    console.log("added employee");
-    // WHEN I choose to add an employee
-    // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+    inquirer.prompt([{
+        type: 'input',
+        message: "What is Employee's first name?",
+        name: 'first'
+    }, {
+        type: 'input',
+        message: "What is Employees last name?",
+        name: 'last'
+    }, {
+        type: 'list',
+        message: "What is Employee role?",
+        choices: [
+            'Clinic Manager',
+            'Nurse',
+            'Medical Assistant',
+            'Sonography Manager',
+            'Sonographer'
+        ],
+        name: 'role'
+    }, {
+        type: 'list',
+        message: "Who is the manager of this Employee?",
+        choices: [
+            'Mindy Christensen',
+            'Xi Chen'
+        ],
+        name: 'manager'
+    }
+    ]).then((data) => {
+        setRoleIDandManagerID(data);
+        db.query(`INSERT INTO employees (${data.first}, ${data.last}, ${roleID}, ${manager}`), (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.table(result);
+            menu();
+        }
+    });
+};
+
+function setRoleIDandManagerID(data) {
+    if (data.choices === 'Clinic Manager') {
+        roleID = 1;
+        manager = '';
+        console.log("Clinic Manager picked")
+    }
+    else if (data.choices === 'Nurse') {
+        roleID = 2;
+        manager = '';
+        console.log("Nurse picked")
+    } else if (data.choices === 'Medical Assistant') {
+        roleID = 2;
+        manager = '';
+        console.log("Medical Assistant picked")
+    } else if (data.choices === 'Sonography Manager') {
+        roleID = 3;
+        manager = '';
+        console.log("Sonography Manager picked")
+    } else if (data.choices === 'Sonographer') {
+        roleID = 3;
+        manager = '';
+        console.log("Sonographer picked")
+    }
 };
 
 function updateEmployee() {
@@ -80,9 +153,13 @@ function updateEmployee() {
 };
 
 function viewAllRoles() {
-    console.log("viewing all roles");
-    // WHEN I choose to view all roles
-    // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+    db.query(`SELECT * FROM roles;`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+        menu();
+    });
 };
 
 function addRole() {
@@ -117,8 +194,5 @@ app.use((req, res) => {
 
 // ====================Listening to port====================
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`)
 });
-
-
-// You might also want to make your queries asynchronous. MySQL2 exposes a .promise() function on Connections to upgrade an existing non-Promise connection to use Promises. To learn more and make your queries asynchronous, refer to the npm documentation on MySQL2Links to an external site..
